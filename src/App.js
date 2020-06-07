3c61585d2ba04e0cb4378febdd44c2cbf74ac38b
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-07 17:26:20 +0500

    Assignment done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App({ name }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myContainer">
+      <div>
+        <h2>Hello World!</h2>
+        <h2>Hello from {name}</h2>
+        <h2>
+          <strong>{name}</strong>
+        </h2>
+      </div>
+      <div>
+        <h3>Some Fruits:</h3>
+        <ul>
+          <li>Orange.</li>
+          <li>Banana.</li>
+          <li>Mango.</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Here is the Js expression: {10 + 5}</h2>
+      </div>
     </div>
   );
 }
